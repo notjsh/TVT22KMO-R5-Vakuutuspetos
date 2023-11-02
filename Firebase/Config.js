9a5d70076f215fb272a26b0a22cf736ae6f893ee
saattaja <janita.elisa@outlook.com>
2023-11-02 20:30:38 +0200

    Update Config.js

--- Firebase/Config.js
+++ Firebase/Config.js
@@ -2,12 +2,7 @@ import {initializeApp} from 'firebase/app'
 import {getFirestore} from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCkylcHDgolJ45M4LAaBwfK_-YKJNpbEJI",
-    authDomain: "vakuutuspetos-574a9.firebaseapp.com",
-    projectId: "vakuutuspetos-574a9",
-    storageBucket: "vakuutuspetos-574a9.appspot.com",
-    messagingSenderId: "716134935095",
-    appId: "1:716134935095:web:8f763e129d6c8383d6edb2"
+   
   };
   
   // Initialize Firebase
